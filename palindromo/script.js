eaadb867903ac9b3db75c0cf0fbce96775ed00a3
NicolaDiQuinzio <nicola.di.quinzio1@gmail.com>
2023-03-17 09:42:01 +0100

    add delivery

--- palindromo/script.js
+++ palindromo/script.js
@@ -1,3 +1,6 @@
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
 function verificaPalindroma() {
   let parola = document.getElementById("parola").value;
   let risultato = document.getElementById("risultato");
--- pari-dispari/script2.js
+++ pari-dispari/script2.js
@@ -1,4 +1,10 @@
 function gioca(scelta) {
+  /*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    Sommiamo i due numeri
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    Dichiariamo chi ha vinto.*/
+
   // Genera un numero random per l'utente e il computer
   let numero = Math.floor(Math.random() * 5) + 1;
   let numeroComputer = Math.floor(Math.random() * 5) + 1;
